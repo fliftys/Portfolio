c9032a37d84f1a29da638a4909c85b65f2dd2cd3
fliftys <fliftys@gmail.com>
2019-09-23 09:20:26 +0200

    Build version for netlify

---
+++ build/precache-manifest.260083c01945aee90a984b36564529f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddbfc2d36370dcb5bed2944bfd75050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abdbaeaca826a8f4a3da",
+    "url": "/static/css/main.6d01159b.chunk.css"
+  },
+  {
+    "revision": "9bf3d8b7f9b911b307c4",
+    "url": "/static/js/2.8e63abeb.chunk.js"
+  },
+  {
+    "revision": "abdbaeaca826a8f4a3da",
+    "url": "/static/js/main.07b06d2b.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "50b6764a5fe5d15d2ad1dd148cda7ffc",
+    "url": "/static/media/avatar.50b6764a.svg"
+  },
+  {
+    "revision": "a43ad4e4f41e344a120f81b525c36d26",
+    "url": "/static/media/bikeshop.a43ad4e4.png"
+  },
+  {
+    "revision": "82daceeb95459aa6a050cf93cfdb3760",
+    "url": "/static/media/challenge11.82daceeb.png"
+  },
+  {
+    "revision": "9180a50b6d34c3de014082e15d7cb4df",
+    "url": "/static/media/challenge32.9180a50b.png"
+  },
+  {
+    "revision": "020e80ec696f6e10fd6671c3d8edd365",
+    "url": "/static/media/photoshop.020e80ec.png"
+  },
+  {
+    "revision": "b489aba16429076599418987cb573875",
+    "url": "/static/media/polind.b489aba1.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "c13c393f310c02a422138de8d2ba4f31",
+    "url": "/static/media/sass.c13c393f.png"
+  },
+  {
+    "revision": "92357ff83d959b411c04558295f392a4",
+    "url": "/static/media/vs.92357ff8.png"
+  }
+]);
